7c9dcf031f709472322d7f25a2cd13ee35abf6ba
QuocTuan2309 <phamquoctuan23092006@gmail.com>
2025-12-04 00:15:34 +0700

    Delete bai4_17.py

--- bai4_17.py
+++
@@ -1,7 +0,0 @@
-print ("Sinh vien: Pham Quoc Tuan")
-print ("Ma so sv: 245752021610029")
-chuoi = input("Nhập chuỗi các số, cách nhau bằng dấu phẩy: ")
-ds_chuoi = chuoi.split(',')
-ds_duong = [int(x.strip()) for x in ds_chuoi if int(x.strip()) > 0]
-print("Danh sách các số nguyên dương là:")
-print(ds_duong)
