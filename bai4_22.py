186bf29d417f2da1b5940e6a762451b2f2f000cc
QuocTuan2309 <phamquoctuan23092006@gmail.com>
2025-12-03 23:49:52 +0700

    Add files via upload

---
+++ bai4_22.py
@@ -0,0 +1,9 @@
+print ("Sinh vien: Pham Quoc Tuan")
+print ("Ma so sv: 245752021610029")
+def all_digits_even(num):
+    for digit in str(num):
+        if int(digit) % 2 != 0:
+            return False
+    return True
+even_digit_numbers = [str(i) for i in range(1000, 3001) if all_digits_even(i)]
+print(",".join(even_digit_numbers))
