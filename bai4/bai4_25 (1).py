efa8c67a94e75c30544e1faa8edce696c17adfbe
QuocTuan2309 <phamquoctuan23092006@gmail.com>
2025-12-04 00:31:04 +0700

    Add files via upload

---
+++ bai4/bai4_25 (1).py
@@ -0,0 +1,6 @@
+print ("Sinh vien: Pham Quoc Tuan")
+print ("Ma so sv: 245752021610029")
+s = input("Nhập danh sách số, cách nhau bởi dấu phẩy: ")
+numbers = [int(x) for x in s.split(",")]
+odd_numbers = [x for x in numbers if x % 2 != 0]
+print("Các số lẻ là:", odd_numbers)
