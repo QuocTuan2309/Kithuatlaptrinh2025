b2336f71990b22238f671610296a127e8b89edf6
QuocTuan2309 <phamquoctuan23092006@gmail.com>
2025-12-04 00:22:59 +0700

    Delete bai6_5.py

--- bai6_5.py
+++
@@ -1,7 +0,0 @@
-print ("Sinh vien: Pham Quoc Tuan")
-print ("Ma so sv: 245752021610029")
-class py_solution:
-    def reverse_words(self, s):
-        return ' '.join(reversed(s.split()))
-sol = py_solution()
-print(sol.reverse_words("hello .py"))   
